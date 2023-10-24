1d8e859d6e35f3037736e88f08873db634d25cbd
gretazas <63977685+gretazas@users.noreply.github.com>
2023-10-24 21:40:26 +0000

    Revert "Add checkout page"
    This reverts commit f7fc290ae81fb1a020987b8b5834c53c155b3fc2.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Checkout from './Checkout';
+import Header from './Header'
+import Home from './Home'
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     // BEM
-    <Router>
-      <div className="app">
-        <Routes>
-        <Route exact path="/" element={<><Header/><Home/></>}/>
-        <Route exact path="/checkout" element={<><Checkout/></>}/> 
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
   );
 }
 
