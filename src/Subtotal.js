a9d7a39f762e0c29055fef048a073cffeb6d4eb5
gretazas <63977685+gretazas@users.noreply.github.com>
2023-10-24 21:14:27 +0000

    Revert last comit

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,7 +1,7 @@
 import { StopRounded } from '@mui/icons-material'
 import React from 'react'
 import CurrencyFormat from 'react-currency-format'
-import './Subtotal.css '
+// import './Subtotal.css '
 
 function Subtotal() {
   return (
@@ -10,7 +10,7 @@ function Subtotal() {
             renderText={(value) => (
                 <>
                 <p>
-                Subtotal (0items): <strong>0</strong>
+                Subtotal (0 items): <strong>0</strong>
                 </p>
                 <small className='subtotal__gift'>
                     <input type='checkbox' /> This order contains a gift
